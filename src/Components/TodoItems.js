4ed9ac35d038c1c1eb740001aa282b18bf68fd35
Neha Hegde <neha.hegde2000@gmail.com>
2021-06-20 22:36:05 +0530

    added styles

--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -2,10 +2,17 @@ import React from "react";
 
 const TodoItems = (props) => {
     const {text,completed} = props.todo;
+
+    const completedItems = {
+        fontStyle : "italic",
+        color : "grey",
+        textDecoration : "line-through"
+    }
+
     return (
         <div className="todo-item">
             <input onChange={()=>props.handleChange(props.todo.id)} type="checkbox" checked={completed} />
-            <span>{text}</span><br />
+            <span style={props.todo.completed ? completedItems : null}>{text}</span><br />
         </div>
     );
 }
